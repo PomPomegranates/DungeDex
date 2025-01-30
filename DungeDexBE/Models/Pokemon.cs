d1d4fbadaee405de135449906121cb4e9a4617a9
digydigdogdead <carlyle.laurent@gmail.com>
2025-01-30 09:55:23 +0000

    Add pokemon object

---
+++ DungeDexBE/Models/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿namespace DungeDexBE.Models
+{
+	public class Pokemon
+	{
+		public string Name { get; set; }
+		public int HP {  get; set; }
+		public int Attack { get; set; }
+		public int Defense { get; set; }
+		public int SpecialAttack { get; set; }
+		public int SpecialDefense { get; set; }
+		public int Speed { get; set; }
+
+		#region stretch
+		//public string Type1 { get; set; }
+		//public string? Type2 { get; set; }
+		#endregion
+
+	}
+}
