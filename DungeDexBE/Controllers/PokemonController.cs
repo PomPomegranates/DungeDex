d8eb9fd93fa482a79d97bf7c71a008ca085d4c47
DrBenSullivan <ben.sullivan1992@gmail.com>
2025-01-30 09:38:55 +0000

    Add HttpClient service.

---
+++ DungeDexBE/Controllers/PokemonController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DungeDexBE.Controllers
+{
+	[ApiController]
+	public class PokemonController
+	{
+	}
+}
---
+++ DungeDexBE/Repositories/PokeApi.cs
@@ -0,0 +1,19 @@
+﻿namespace DungeDexBE.Repositories
+{
+	public class PokeApi
+	{
+		private readonly HttpClient _httpClient;
+
+		public PokeApi(IHttpClientFactory httpClientFactory)
+		{
+			_httpClient = httpClientFactory.CreateClient("pokemon");
+		}
+
+		public async Task<string> GetPokemon()
+		{
+			using var httpClient = _httpClient;
+			string result = await httpClient.GetStringAsync("pokemon/1");
+			return result;
+		}
+	}
+}
