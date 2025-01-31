d30468ccd53969286d968b0311126c2293c5448a
PomPomegranates <persephoneapplewhite@gmail.com>
2025-01-31 12:30:39 +0000

    ToDo: CorsPolicy Functionality

--- DungeDexFE/DungeDexFE/Program.cs
+++ DungeDexFE/DungeDexFE/Program.cs
@@ -12,11 +12,19 @@ namespace DungeDexFE
 			builder.Services.AddRazorComponents()
 				.AddInteractiveServerComponents()
 				.AddInteractiveWebAssemblyComponents();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost",
+                    policy => policy.WithOrigins("http://localhost:7107")
+                                    .AllowAnyMethod()
+                                    .AllowAnyHeader());
+            });
 
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
+            
+            var app = builder.Build();
+            
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
 			{
 				app.UseWebAssemblyDebugging();
 			}
@@ -26,10 +34,12 @@ namespace DungeDexFE
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
-
+			
 			app.UseHttpsRedirection();
 
-			app.UseStaticFiles();
+            app.UseCors("AllowLocalHost");
+
+            app.UseStaticFiles();
 			app.UseAntiforgery();
 
 			app.MapRazorComponents<App>()
