63de61706e978f3afe8a906a5358df2c5b595461
PomPomegranates <persephoneapplewhite@gmail.com>
2025-02-04 10:32:46 +0000

    Second Database Create - Issue with List of User Creations

--- DungeDexBE/Models/User.cs
+++ DungeDexBE/Models/User.cs
@@ -4,7 +4,8 @@
     {
         public  int Id { get; set; }
         public string UserName { get; set; } = null!;
-        public virtual List<DungeMon> Creations { get; set; } = [];
+        public List<int> DungeMonIds { get; set; } = new List<int>();
+        public virtual List<DungeMon> DungMons { get; set; } = [];
 
     }
 }
--- DungeDexBE/Program.cs
+++ DungeDexBE/Program.cs
@@ -80,51 +80,122 @@ namespace DungeDexBE
 			app.Run();
 		}
 
-		static void AddData(WebApplication app)
-		{
-			var scope = app.Services.CreateScope();
-			var db = scope.ServiceProvider.GetService<MyDbContext>();
-			
-			
-
-			db.Database.EnsureDeleted();
-			db.Database.EnsureCreated();
-
-			var spell = new Spell
-			{
-				Name = "Boom",
-				Description = "Big Boom"
-			};
-
-			var spell2 = new Spell
-			{
-				Name = "ah",
-				Description = "oh no"
-			};
+        static void AddData(WebApplication app)
+        {
+            var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetService<MyDbContext>();
 
 
-			var monster = new Monster
-			{
-				
-				BasePokemon = "Jim",
-				ChallengeRating = 12,
-				ArmorClass = 12,
-                HitPoints = 1,
-				Strength = 3,
-				Constitution = 3,
-				Wisdom = 3,
-				Intelligence = 3,
-				Dexterity = 3,
-				Charisma = 3,
-				Spells = new List<Spell>
-				{
-					spell,
-					spell2
-				},
-				ImageLink = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/186.png"
-			};
 
-			db.MonsterDb.Add(monster);
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+
+            var user = new User
+            {
+                UserName = "Testina"
+
+
+            };
+            var user2 = new User
+            {
+                UserName = "John Doe"
+
+
+            };
+
+            var spell = new Spell
+            {
+                Name = "Goodberry",
+                Description = "Up to ten berries appear in your hand and are infused with magic for the duration. A creature can use its action to eat one berry. Eating a berry restores 1 hit point, and the berry provides enough nourishment to sustain a creature for a day."
+
+            };
+
+            var spell2 = new Spell
+            {
+                Name = "Sunbeam",
+                Description = "A beam of brilliant light flashes out from your hand in a 5-foot-wide, 60-foot-long line. Each creature in the line must make a constitution saving throw. On a failed save, a creature takes 6d8 radiant damage and is blinded until your next turn. On a successful save, it takes half as much damage and isn't blinded by this spell. Undead and oozes have disadvantage on this saving throw."
+            };
+
+            var spell3 = new Spell
+            {
+                Name = "Acid Splash",
+                Description = "You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a dexterity saving throw or take 1d6 acid damage."
+            };
+
+
+        var monster = new DungeMon
+            {
+
+                BasePokemon = "Lilligant",
+                NickName = "Lilly",
+                User = user,
+                ChallengeRating = 12,
+                ArmorClass = 12,
+                HitPoints = 100,
+                Strength = 14,
+                Constitution = 13,
+                Wisdom = 14,
+                Intelligence = 12,
+                Dexterity = 18,
+                Charisma = 9,
+                Spells = new List<Spell>
+                {
+                    spell,
+                    spell3
+                },
+                ImageLink = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/186.png"
+            };
+
+            var monster2 = new DungeMon
+            {
+
+                BasePokemon = "Venusaur",
+                NickName = "Jimmy",
+                User = user,
+                ChallengeRating = 12,
+                ArmorClass = 12,
+                HitPoints = 100,
+                Strength = 14,
+                Constitution = 13,
+                Wisdom = 14,
+                Intelligence = 12,
+                Dexterity = 18,
+                Charisma = 9,
+                Spells = new List<Spell>
+                {
+                    spell,
+                    spell2
+                },
+                ImageLink = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/186.png"
+            };
+            var monster3 = new DungeMon
+            {
+
+                BasePokemon = "Bayleaf",
+                NickName = "Baybeee",
+                User = user2,
+                ChallengeRating = 12,
+                ArmorClass = 12,
+                HitPoints = 100,
+                Strength = 14,
+                Constitution = 13,
+                Wisdom = 14,
+                Intelligence = 12,
+                Dexterity = 18,
+                Charisma = 9,
+                Spells = new List<Spell>
+                {
+                    spell2,
+                    spell3
+                },
+                ImageLink = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/186.png"
+            };
+
+
+            db.MonsterDb.Add(monster);
+            db.MonsterDb.Add(monster2);
+            db.MonsterDb.Add(monster3);
 
             db.SaveChanges();
         }
